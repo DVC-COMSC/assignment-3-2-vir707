0e43359d506b8c5569f1253c80724660eb6823e2
vir707 <126916752+vir707@users.noreply.github.com>
2023-03-20 06:31:28 +0000

    complete

--- main_test.py
+++ main_test.py
@@ -78,3 +78,13 @@ def test_main_4():
     res = re.search(r'[\w,\W]*[f,F]alse[\w,\W]*', lines[0])
     assert res != None
     print(res.group())
+ n1 = random.randint(0,100)
+    n2 = random.randint(0,100)
+    n3 = random.randint(0,100)
+    print (n1, n2, n3)
+    if n1 < n2 and n1 < n3:
+        print (f'{n1} is the smallest number')
+    elif n2 < n1 and n2 < n3:
+        print (f'{n2} is the smallest number')
+    else:
+        print (f'{n3} is the smallest number')
